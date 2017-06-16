b36de4e6e247fd2f20ee494cb38906fb26d61ed3
HJH <HJH@DESKTOP-A3AQ5LG>
2017-06-16 16:48:30 +0900

    123

--- src/main/java/com/jx372/mysite/controller/MainController.java
+++ src/main/java/com/jx372/mysite/controller/MainController.java
@@ -3,6 +3,7 @@ package com.jx372.mysite.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
