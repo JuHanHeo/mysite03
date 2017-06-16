731e602781118d5559dcb3244e66803819082f66
HJH <HJH@DESKTOP-A3AQ5LG>
2017-06-16 15:45:22 +0900

    pathparam -> pathvariable

--- src/main/java/com/jx372/mysite/controller/GuestBookController.java
+++ src/main/java/com/jx372/mysite/controller/GuestBookController.java
@@ -1,12 +1,12 @@
 package com.jx372.mysite.controller;
 
 
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,12 +35,12 @@ public class GuestBookController {
 	}
 	
 	@RequestMapping("/deleteform")
-	public String deleteform(@PathParam("no") String no){
+	public String deleteform(@PathVariable("no") String no){
 		return "/guestbook/deleteform";
 	}
 	
 	@RequestMapping(value = "/delete", method = RequestMethod.POST)
-	public String delete(@PathParam("no") String no){
+	public String delete(@PathVariable("no") String no){
 		
 		guestBookService.delete(no);
 		
